8e5d2b5d2ac27c7b64cab58f29e74e5d8cead78a
Deeksha Tiwari <tdeeksha617@gmail.com>
2023-10-31 15:16:35 +0530

    resolving some error in file_viewer.js

--- assets/js/file_viewer.js
+++ assets/js/file_viewer.js
@@ -1,25 +1,31 @@
-console.log('Im in');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the search input and table
+  const searchInput = document.getElementById("search-input");
+  const table = document.querySelector("table");
+  // console.log('Im in');
 
+  document.getElementById("go").addEventListener("click", searchTable);
 
-document.getElementById("go").addEventListener("click", searchTable);
+  function searchTable() {
+    // Get the input value and convert it to lowercase
+    const input = searchInput.value.toLowerCase();
 
-function searchTable() {
-  // Get the input value and convert it to lowercase
-  const input = document.getElementById("search-input").value.toLowerCase();
-  console.log(input);
-  
-  // Get all table rows and iterate through them
-  const rows = document.getElementsByTagName("tr");
+    // Get all table rows and iterate through them
+    const rows = table.getElementsByTagName("tr");
 
-  for (let i = 0; i < rows.length; i++) {
-    // Get the text content of each row and convert it to lowercase
-    const rowText = rows[i].textContent.toLowerCase();
-    
-    // If the input value is found in the row text, highlight the row
-    if (rowText.includes(input)) {
-      rows[i].classList.add("highlight");
-    } else {
-      rows[i].classList.remove("highlight");
+    for (let i = 0; i < rows.length; i++) {
+      // Get the text content of each row and convert it to lowercase
+      const rowText = rows[i].textContent.toLowerCase();
+
+      // If the input value is found in the row text, highlight the row
+      if (rowText.includes(input)) {
+        rows[i].classList.add("highlight");
+      } else {
+        rows[i].classList.remove("highlight");
+      }
     }
   }
-}
+});
+
+
+
