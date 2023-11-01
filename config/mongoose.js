0743415241df9417db5c79672e0ad0c0ff976371
Deeksha Tiwari <tdeeksha617@gmail.com>
2023-11-01 13:39:14 +0530

    db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,34 +1,28 @@
-// IMPORTING PACKAGE
 const mongoose = require("mongoose");
 
+const DB = 'mongodb://localhost/CSV_file_Upload';
 
-
-// MAKING CONNECTION 
-
-// Define the database connection URL
-const DB = 'mongodb://0.0.0.0:27017/CSV_file_Upload';
-
-// Attempt to connect to the MongoDB database
-mongoose.connect(DB).then(() => {
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      family: 4
+    });
     console.log('Connected to server successful!');
-}).catch((err) => console.log("No connection " + err));
+  } catch (err) {
+    console.error("No connection " + err);
+  }
+}
 
-// Set the connection to a variable
 const db = mongoose.connection;
 
-
-
-// CHECKING CONNECTION
-
-// Handle errors if a connection error occurs
 db.on("error", console.error.bind(console, "Error connecting to DB"));
 
-// Handle the event when the database connects successfully
 db.once("open", function() {
-    console.log("Successfully connected to Database");
+  console.log("Successfully connected to Database");
 });
 
-// EXPORTING DB 
-
-// Export the 'db' variable so that it can be used in other parts of your application
 module.exports = db;
+
+connectToDatabase();
